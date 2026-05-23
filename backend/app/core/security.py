e85ef21ba157ba79efdf2272451871c316182528
Official-Vicks <chidieberevictory218@gmail.com>
2026-05-23 01:19:16 +0100

    Add files via upload

--- backend/app/core/security.py
+++ backend/app/core/security.py
@@ -6,9 +6,15 @@ from app.core.config import SECRET_KEY, ALGORITHM, ACCESS_TOKEN_EXPIRE_MINUTES
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 def hash_password(password: str):
+    if len(password.encode("utf-8")) > 72:
+        raise ValueError("Password too long")
+
     return pwd_context.hash(password)
 
 def verify_password(plain, hashed):
+    if len(plain.encode("utf-8")) > 72:
+        raise ValueError("Password too long")
+
     return pwd_context.verify(plain, hashed)
 
 def create_access_token(data: dict):
