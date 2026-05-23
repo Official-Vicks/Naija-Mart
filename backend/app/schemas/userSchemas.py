ed2e6cdadfc05f91101b94fcf5f745633781bd80
Official-Vicks <chidieberevictory218@gmail.com>
2026-05-23 01:19:59 +0100

    Add files via upload

--- backend/app/schemas/userSchemas.py
+++ backend/app/schemas/userSchemas.py
@@ -1,11 +1,17 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, field_validator
 
 class UserCreate(BaseModel):
     email: str
-    password: str
     full_name: str
     store_name: str
     phone: str
+    password: str
+
+    @field_validator("password")
+    def validate_password(cls, v):
+        if len(v.encode("utf-8")) > 72:
+            raise ValueError("Password too long")
+        return v
 
 class UserResponse(BaseModel):
     id: str
